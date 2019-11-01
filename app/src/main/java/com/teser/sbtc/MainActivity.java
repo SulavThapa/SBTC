a26e32925bc3afa8692ca1a4e09f845b36997273
sulav_21 <thapasulup@gmail.com>
2019-11-01 15:44:38 +0545

    First Commit

---
+++ app/src/main/java/com/teser/sbtc/MainActivity.java
@@ -0,0 +1,32 @@
+package com.teser.sbtc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button enter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        enter = (Button) findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainPage();
+            }
+        });
+    }
+
+    private void mainPage() {
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
+    }
+}
