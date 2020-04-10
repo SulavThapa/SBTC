67c6dcaf7c8e160a29e04381317ea8e49e5f6e4a
SulavThapa <46512631+SulavThapa@users.noreply.github.com>
2020-04-10 09:14:49 +0545

    Updates in Gradle

--- app/src/main/java/com/teser/sbtc/MainActivity.java
+++ app/src/main/java/com/teser/sbtc/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -21,11 +22,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     public static String BaseUrl = "https://api.thingspeak.com/";
+    public static String lat, lon;
 
     private Button enter;
     private EditText username;
     private final String CHANNEL_ID = "notification";
     private final int NOTIFICATION_ID = 001;
+//    public String lat, lon;
 
 
     @Override
@@ -56,14 +59,25 @@ public class MainActivity extends AppCompatActivity {
             System.out.println(generatedSecuredPasswordHash);
 
             Log.i("When Button is clicked", "-----------------------------------------------");
+            Log.i("Displaying username", username.getText().toString());
 
+            String code = username.getText().toString();
+            if(code.equals("tracker")){
+                Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+            else {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Incorrect code!! Enter the valid code",
+                        Toast.LENGTH_SHORT);
 
-            //Intent intent = new Intent(MainActivity.this, MapsActivity.class);
-            //startActivity(intent);
+                toast.show();
+            }
+
         });
     }
 
-    private void LogingUser() {
+    public void LogingUser() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BaseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -86,8 +100,8 @@ public class MainActivity extends AppCompatActivity {
                     int ss = mainChannel.feeds.size();
                     for (int i = 0; i < ss; i++) {
 
-                        String lat = mainChannel.feeds.get(i).getField1() + " ";
-                        String lon = mainChannel.feeds.get(i).getField2() + " ";
+                        lat = mainChannel.feeds.get(i).getField1() + " ";
+                        lon = mainChannel.feeds.get(i).getField2() + " ";
                         Log.i("Start", "-----------------------------------------------");
                         Log.i("Output", lat + " " + lon);
                         Log.i("End", "-----------------------------------------------");
